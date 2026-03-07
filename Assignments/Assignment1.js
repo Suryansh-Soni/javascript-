2113414b7d1edc5fc673994ccddc816e98fc8e40
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-07 13:28:50 +0530

    Assignments

---
+++ Assignments/Assignment1.js
@@ -0,0 +1,23 @@
+// Task1:create program to genetrate random dice roll
+const roll=Math.floor(Math.random()*6)+1
+console.log("The number is :",roll)
+
+// Task2:create obj rep a car 
+const car={
+    name:"honda city",
+    model:"Base",
+    color:"white",
+}
+console.log(car.name)
+
+// Task3:create obj person and edit its city and add new prop country 
+
+const Person={
+    name:"Rakesh singh",
+    age:18,
+    city:"nagpur",
+}
+
+Person.city="New York"
+Person.country="United States"
+console.log(Person)
