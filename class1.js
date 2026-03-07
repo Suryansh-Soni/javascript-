db8c3fc97183103ed031d1e617b95e996eca098b
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-07 12:53:24 +0530

    class 1 :basics

---
+++ class1.js
@@ -0,0 +1,79 @@
+// function sum(...arg) {
+//   let total = 0;
+//   for (let i = 0; i < arg.length; i++) {
+//     total += arg[i];
+//   }
+//   console.log(total);
+// }
+// function min(...args) {
+//   return args.reduce((min, el) => {
+//     return el < min ? el : min;
+//   });
+// }
+// console.log(min(1, 2, 3, 4, 5));
+// sum(1, 2, 3, 4, 5);
+// console.log("hello world");
+// a = 1;
+// y = a++ * ++a;
+// console.log(y);
+
+// let str = "hello";
+
+// console.log(str.toUpperCase());
+// console.log(str.indexOf("o"));
+// console.log(str.slice(1, 4));
+// console.log(str.repeat(3));
+// console.log(str.replace("l", "x"));
+
+// let info=["suryansh",49,'c'];
+// let info = new Array("suryansh", 49, "c");
+// info.push(6);
+// info.unshift(1);
+// info.shift();
+// console.log(info);
+// console.log(info.indexOf("suryansh"));
+// info.splice(1, 1, 0, "soni");
+// console.log(info);
+// let sq = [1, 2, 9, 16, 36, 25, 49, 64, 81, 100];
+// console.log(sq.sort((a, b) => a - b));
+
+// shallow copy
+// arr1 = [1, 2];
+// arrc = arr1;
+// console.log(arrc == arr1);
+// arr1.push(3);
+// console.log(arrc);
+
+// arr1 = [1, 2];
+// arrc = [1, 2];
+// console.log(arrc == arr1);
+// arr1 = [
+//   [1, 2],
+//   [3, 4],
+// ];
+// console.log(arr1[0][1]);
+
+// arr = [1, 2, 3, 4, 5];
+// n = -3;
+// console.log(arr.slice(n));
+
+// arr = [];
+// if (arr.length == 0) {
+//   console.log("array is empty");
+// }
+
+// str = "Suryansh";
+// if (str[0] == str[0].toUpperCase()) {
+//   console.log("first letter is capital");
+// }
+
+// str = "     hello   ";
+// console.log(str.trim());
+
+// 
+
+
+
+
+
+ 
