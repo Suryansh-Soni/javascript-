5e25da4b766ffa1e61a44889a13923ffbf393a30
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-08 14:33:30 +0530

    methods in js

---
+++ method.js
@@ -0,0 +1,17 @@
+const calculator = {
+  add: function (a, b) {
+    return a + b;
+  },
+  sub: function (a, b) {
+    return a - b;
+  },
+  mult: function (a, b) {
+    return a * b;
+  },
+  div: function (a, b) {
+    return a / b;
+  },
+};
+
+console.log(calculator.add(3, 4));
+
