ebef3a0e47fda713a27e072fed4877ea1b08d07c
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-08 15:41:06 +0530

    tempcoderunner

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+const student = {
+//   name: "suryansh",
+//   age: 21,
+//   getage() {
+//     console.log(this);
+//     let age = this.age;
+//     console.log(`${age} is just a number`);
+//   },
+// };
+// student.getage();
