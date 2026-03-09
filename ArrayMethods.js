4b3502890ab803b9a36d2646eb3c3c9683a08387
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-09 16:26:28 +0530

    some functiona dn every function

--- ArrayMethods.js
+++ ArrayMethods.js
@@ -21,9 +21,15 @@
 
 // console.log(double);
 
-// filter
-let num = [1, 2, 3, 4, 5];
-let double = num.filter((el) => {
-  return el % 2 == 0;
-});
-console.log(double);
+// // filter
+// let num = [1, 2, 3, 4, 5];
+// let double = num.filter((el) => {
+//   return el % 2 == 0;
+// });
+// console.log(double);
+
+// "every" similer to and operator
+console.log([2, 3, 4].every((el) => el % 2 == 0));
+
+// some function simiter to or
+console.log([2, 3, 4].some((el) => el % 2 == 0));
