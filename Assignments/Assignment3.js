5c898795482bffb629813b759d5d0a6c289ce713
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-09 15:57:15 +0530

    Task 2 completed

--- Assignments/Assignment3.js
+++ Assignments/Assignment3.js
@@ -4,8 +4,18 @@ const avg = (arr) => {
   for (let i of arr) {
     sum += i;
   }
-  return  sum / arr.length;
+  return sum / arr.length;
 };
 
 let arr = [1, 2, 3, 4];
 console.log(avg(arr));
+
+// Task2:
+let even = (n) => {
+  if (n % 2 == 0) console.log(`${n} is even`);
+  else {
+    console.log(`${n} is odd`);
+  }
+};
+
+even(48);
