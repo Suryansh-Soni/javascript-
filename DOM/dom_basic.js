94d13d91caeb59936a2b29c48b355dde4b3af22b
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-12 13:21:37 +0530

    dom basixs

---
+++ DOM/dom_basic.js
@@ -0,0 +1,23 @@
+//   get element by id
+// do in console in inspect 
+
+
+// let img = document.getElementsByClassName("oldImg");
+// for (let i = 0; i < img.length; i++) {
+//   console.dir(img[i].src());
+// } 
+
+// get element by tag
+document.getElementsByTagName("p")[1].innerText="abc"
+
+// query selector 
+document.querySelector('p') //selects first p element
+document.querySelector('div a') //first ancor tag in div 
+document.querySelector('#myId') //select first element with id=myId
+document.querySelector('.myClass')
+document.querySelectorAll('p') //select all p elements,give node list 
+
+
+
+
+ 
