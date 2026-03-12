1eedde3a35306818fb99f340e4d1b3a992cf0f6a
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-12 17:55:52 +0530

    dom basics

--- DOM/dom_basic.js
+++ DOM/dom_basic.js
@@ -1,23 +1,18 @@
 //   get element by id
-// do in console in inspect 
-
+// do in console in inspect
 
 // let img = document.getElementsByClassName("oldImg");
 // for (let i = 0; i < img.length; i++) {
 //   console.dir(img[i].src());
-// } 
+// }
 
 // get element by tag
-document.getElementsByTagName("p")[1].innerText="abc"
+document.getElementsByTagName("p")[1].innerText = "abc";
 
-// query selector 
-document.querySelector('p') //selects first p element
-document.querySelector('div a') //first ancor tag in div 
-document.querySelector('#myId') //select first element with id=myId
-document.querySelector('.myClass')
-document.querySelectorAll('p') //select all p elements,give node list 
-
-
-
-
- 
+// query selector
+document.querySelector("p"); //selects first p element
+document.querySelector("div a"); //first ancor tag in div
+let doc = document.querySelector("#myId"); //select first element with id=myId
+document.querySelector(".myClass");
+document.querySelectorAll("p"); //select all p elements,give node list
+console.log(doc);
