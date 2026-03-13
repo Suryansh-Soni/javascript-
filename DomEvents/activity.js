bae2f7d5a144f48759f270b2115341921f1be2b2
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-13 14:54:40 +0530

    activity : generate random Color

---
+++ DomEvents/activity.js
@@ -0,0 +1,22 @@
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+  let h1 = document.querySelector("h1");
+  let randomcolor = Randomcolor();
+
+  h1.innerText = randomcolor;
+
+  let div = document.querySelector("div");
+  div.style.backgroundColor = randomcolor;
+
+  console.log("color updated");
+});
+
+function Randomcolor() {
+  let green = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
+  let red = Math.floor(Math.random() * 255);
+
+  let color = `rgb(${red}, ${green}, ${blue})`;
+  return color;
+}
