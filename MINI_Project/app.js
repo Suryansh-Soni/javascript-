12f062320180096d547fbc082d01c1f0966d8c45
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-15 13:31:26 +0530

    added blink effect by game and by user

---
+++ MINI_Project/app.js
@@ -0,0 +1,55 @@
+let h2 = document.querySelector("h2");
+
+let btns = ["yellow", "red", "purple", "green"];
+let gameSeq = [];
+let userSeq = [];
+let started = false;
+let level = 0;
+
+document.addEventListener("keypress", function () {
+  if (started == false) {
+    console.log("Game Started");
+    started = true;
+    levelUp(); // start first level
+  }
+});
+
+function gameFlash(btn) {
+  btn.classList.add("flash");
+
+  setTimeout(function () {
+    btn.classList.remove("flash");
+  }, 100);
+}
+
+function userFlash(btn) {
+  btn.classList.add("userFlash");
+
+  setTimeout(function () {
+    btn.classList.remove("userFlash");
+  }, 100);
+}
+
+function levelUp() {
+  level++;
+  h2.innerText = `Level : ${level}`;
+
+  let random = Math.floor(Math.random() * 4);
+  let randcolor = btns[random];
+
+  gameSeq.push(randcolor);
+
+  let randombtn = document.querySelector(`.${randcolor}`);
+  gameFlash(randombtn);
+}
+
+function btnPress() {
+  let btn = this;
+  userFlash(btn);
+}
+
+let allbtn = document.querySelectorAll(".btn");
+
+for (let btn of allbtn) {
+  btn.addEventListener("click", btnPress);
+}
